e2917d03b0a1712be76c6da7025774c5a8a00a89
Thomas Levy <tlevy@evernote.com>
2020-04-30 22:18:57 -0700

    generate images in wasm

---
+++ gen_image.c
@@ -0,0 +1,37 @@
+#include "generator.h"
+#include "util.h"
+#include <emscripten.h>
+
+unsigned char* EMSCRIPTEN_KEEPALIVE gen_image(int s) {
+  unsigned char biomeColours[256][3];
+  initBiomes();
+
+  // Initialize a colour map for biomes.
+  initBiomeColours(biomeColours);
+
+  // Extract the desired layer.
+	LayerStack g = setupGenerator(MC_1_15);
+  Layer *layer = &g.layers[L_SHORE_16];
+
+  int areaX = -128, areaZ = -128;
+  unsigned int areaWidth = 256, areaHeight = 256;
+  unsigned int scale = 2;
+  unsigned int imgWidth = areaWidth * scale, imgHeight = areaHeight * scale;
+
+  // Allocate a sufficient buffer for the biomes and for the image pixels.
+  int *biomes = allocCache(layer, areaWidth, areaHeight);
+  unsigned char *rgb = (unsigned char *)malloc(4 * imgWidth * imgHeight);
+
+  // Apply the seed only for the required layers and generate the area.
+  setWorldSeed(layer, s);
+  genArea(layer, biomes, areaX, areaZ, areaWidth, areaHeight);
+
+  // Map the biomes to a color buffer and save to an image.
+  biomesToImage(rgb, biomeColours, biomes, areaWidth, areaHeight, scale, 2, 1);
+
+  // Clean up.
+  free(biomes);
+  freeGenerator(g);
+
+  return rgb;
+}
--- util.h
+++ util.h
@@ -7,7 +7,8 @@ void initBiomeTypeColours(unsigned char biomeColours[256][3]);
 int biomesToImage(unsigned char *pixels, 
         const unsigned char biomeColours[256][3], const int *biomes, 
         const unsigned int sx, const unsigned int sy, 
-        const unsigned int pixscale, const int flip);
+        const unsigned int pixscale, const int flip,
+        int alpha);
 
 int savePPM(const char* path, const unsigned char *pixels, 
         const unsigned int sx, const unsigned int sy);
